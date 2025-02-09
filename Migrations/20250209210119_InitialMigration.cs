c2be4c02ec3e681ce31e28a005153b01246a5810
Ugochukwu Mmaduekwe <ugo4brain@gmail.com>
2025-02-09 22:14:28 +0100

    initial commit

---
+++ Migrations/20250209210119_InitialMigration.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreAsNoTrackingAndJoinBugDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BatchId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionReversals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Reference = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionReversals", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionReversals_Reference",
+                table: "TransactionReversals",
+                column: "Reference",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "TransactionReversals");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+using (var context = new TransactionDbContext())
+{
+    var reference = "TRX12345";
+
+    // Delete the database if it exists
+    context.Database.EnsureDeleted();
+    Console.WriteLine("Database deleted.");
+
+    // Apply migrations and create the database
+    context.Database.Migrate();
+    Console.WriteLine("Database created with migrations.");
+
+
+    // Create and save a new TransactionReversalEntity
+    var transactionReversalEntity = new TransactionReversalEntity
+    {
+        Reference = reference,
+        Name = "Old Name"
+    };
+    context.TransactionReversals.Add(transactionReversalEntity);
+
+    // Create and save a new PaymentEntity
+    var paymentEntity = new PaymentEntity
+    {
+        BatchId = reference
+    };
+    context.Payments.Add(paymentEntity);
+
+    // Save changes to the database
+    context.SaveChanges();
+    Console.WriteLine("Entities have been successfully added to the database.");
+
+    var paymentQuery = context.Payments.AsNoTracking().AsQueryable(); // Disable Tracking for the Payment Query
+    var transactionReversalQuery = context.TransactionReversals.AsQueryable(); // Tracking is still enabled for the Reversal Query
+
+    // Fetch The Reversal and Payment Entity via a Join
+    var reversalPairs = await (from reversal in transactionReversalQuery
+                               join payment in paymentQuery
+                               on reversal.Reference equals payment.BatchId
+                               select new
+                               {
+                                   TransactionReversal = reversal,
+                                   Payment = payment
+                               }).ToListAsync();
+
+    // Get The Reversal Entity
+    var reversalEntity = reversalPairs.First(x => x.TransactionReversal.Reference == reference).TransactionReversal;
+
+    var updatedName = "Updated Name";
+
+    // Update The Name Of The Reversal Entity
+    reversalEntity.Name = updatedName;
+
+    // Save changes to the database
+    context.SaveChanges();
+
+    Console.WriteLine("Entities have been successfully updated in the database.");
+
+    // Fetch The Reversal Entity
+    reversalEntity = await context.TransactionReversals.FirstAsync(x => x.Reference == reference);
+
+    var reversalEntityName = reversalEntity.Name;
+
+    if (string.Equals(reversalEntityName, updatedName, StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("Entities have been successfully updated in the database.");
+    }
+    else
+    {
+        Console.WriteLine($"Entities have not been updated in the database., Name in the DB is {reversalEntityName}");
+    }
+}
+
+
+public class TransactionReversalEntity
+{
+    public int Id { get; set; }
+    public string? Reference { get; set; }
+    public string? Name { get; set; }
+}
+
+public class PaymentEntity
+{
+    public int Id { get; set; }
+    public string? BatchId { get; set; }
+}
+
+public class TransactionDbContext : DbContext
+{
+    public DbSet<TransactionReversalEntity> TransactionReversals { get; set; }
+    public DbSet<PaymentEntity> Payments { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Update the db connection string with yours
+        optionsBuilder.UseSqlServer("Server=localhost;Database=TransactionDemoDb;Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TransactionReversalEntity>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(x => x.Reference).IsUnique();
+            entity.Property(e => e.Reference).HasMaxLength(50).IsRequired();
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<PaymentEntity>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.BatchId).HasMaxLength(50).IsRequired();
+        });
+    }
+}
